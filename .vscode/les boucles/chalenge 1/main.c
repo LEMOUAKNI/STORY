27ef39f29c3ffb2a4545a5d0497e52d116f35010
LEMOUAKNI <lemhamza7@gmail.com>
2024-09-26 14:11:18 +0100

    gg

---
+++ .vscode/les boucles/chalenge 1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    i=5*0;
+    printf("5*0=%d\n",i);
+    i=5*1;
+    printf("5*1=%d\n",i);
+    i=5*2;
+    printf("5*2=%d\n",i);
+    i=5*3;
+    printf("5*4=%d\n",i);
+    i=5*4;
+    printf("5*5=%d\n",i);
+    i=5*6;
+    printf("5*6=%d\n",i);
+    i=5*7;
+    printf("5*7=%d\n",i);
+    i=5*8;
+    printf("5*8=%d\n",i);
+    i=5*9;
+    printf("5*9=%d\n",i);
+    i=5*10;
+    printf("5*10=%d\n",i);
+
+    return 0;
+}
---
+++ .vscode/les boucles/chalenge 3/chalenge 3/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("saisir un nombre :");
+    scanf("%d",&n);
+    if(n<=0)
+        {
+            printf("le nombre que vous avez saisi est un nombre negative");
+
+    }
+
+    return 0;
+}
